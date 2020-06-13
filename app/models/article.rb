f7c02b11d2d6a9c731b75e7fb9645a9c4f926552
Timothy Vieira <vieira.timothy@yahoo.ca>
2020-06-13 18:03:50 -0400

    added a few gems

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,11 @@ class Article < ApplicationRecord
     has_many :comments, dependent: :delete_all
     has_many :taggings, dependent: :delete_all
     has_many :tags, through: :taggings
+    has_many :attachments, dependent: :delete_all
+    has_attached_file :image
+    validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+
     def tag_list
         self.tags.collect do |tag|
           tag.name
